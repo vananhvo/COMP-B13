df36769540aa65ed050ec33484c42f7c04ba554e
Vananh Vo <vananhvo.0108@gmail.com>
2015-10-20 18:58:59 -0700

    Create binaryToDecimal.c
    This file contains the function binaryToDecimal()

---
+++ binaryToDecimal.c
@@ -0,0 +1,36 @@
+/**
+* This file contains the function binaryToDecimal()
+*
+* @author Vananh Vo     
+* Course: COMP B13
+* Created: Sept 4 2015
+* Source File:binaryToDecimal.c
+*/
+ 
+/*
+* Converts the input string of ASCII zeroes and ones in binaryString,
+* to its integer equivalent and returns that integer.
+*
+* @param binaryString A string of ones and zeroes
+* @return the value of the binary bit string
+*/
+ 
+int binaryToDecimal(char *binaryString){
+     
+int decimalNumber =0;
+ 
+int i=0;
+ 
+    while (binaryString[i]!= '\0') {
+         
+        decimalNumber *= 2;
+         
+        if (binaryString[i]=='1'){
+            ++decimalNumber;
+        }
+         
+        i++;
+         
+    }
+    return decimalNumber;
+}
