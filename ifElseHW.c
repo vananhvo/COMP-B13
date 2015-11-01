09f923b142628342f9f9e1b964febf7c7898040d
Vananh Vo <vananhvo.0108@gmail.com>
2015-11-01 13:05:31 -0800

    Update ifElseHW.c

--- ifElseHW.c
+++ ifElseHW.c
@@ -6,7 +6,7 @@
 *	Source: ifElseHW.c
 */
 int main (int argc, char **argv){
-	int argument1 = atoi(argv[0]);
+	int argument1 = atoi(argv[1]);
 	if (argument1 == 4){
 		argument1 = argument1 + 20;
 	} else if (argument1 <= 6 || argument1 > 9){
