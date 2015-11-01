800758e41b973e1f87814478f2ca826616c9ea75
Vananh Vo <vananhvo.0108@gmail.com>
2015-11-01 15:10:44 -0800

    Update ifElseHW.c

--- ifElseHW.c
+++ ifElseHW.c
@@ -5,18 +5,17 @@
 *	Professor: Halrold Mendoza
 *	Source: ifElseHW.c
 */
+#include <stdio.h>
 int main (int argc, char **argv){
 	int argument1 = atoi(argv[1]);
-	if (argument1 == 4){
-		argument1 = argument1 + 20;
-	} else if (argument1 <= 6 || argument1 > 9){
-		if (argument1 != 0){
-			if (argument1 != -1){
-				argument1 = 99;
-			}else{
-				argument1 = 44;
-			}
-		}
+	if (argument1-4 == 0 || (argument1 <= 6  || argument1 < 10)){
+		argument1 = 20;
+	} else if(argument1 == 0){
+		argument1 = 44;
+	} else if (argument1 == -1){
+		argument1 = -44;		
+	} else {
+		argument1 = 99;
 	}
 	printf("%d",argument1);
 	return 0;
