d98f09713c41a190c58dbd778a361ab433b32c0f
Vananh Vo <vananhvo.0108@gmail.com>
2015-11-01 11:12:38 -0800

    Update loopHW.s

--- loopHW.s
+++ loopHW.s
@@ -1,3 +1,5 @@
+#
+
 LC0:
 	.ascii "%d\12\0"
 	.text
@@ -14,4 +16,26 @@ _main:
 	call	_atoi
 	movl	%eax,	24(%esp)
 	movl	12(%ebp),	%eax
-	
+	addl	$8,	%eax
+	movl	(%eax),	%eax
+	movl	%eax,	(%esp)
+	call	_atoi
+	movl	%eax,	20(%esp)
+	movl	%24(%esp),	%eax
+	movl	%eax,	28(%esp)
+	jmp	L2
+L3:
+	movl	28(%esp),	%eax
+	movl	%eax,	4(%esp)
+	movl	$LC0,	(%esp)
+	call	_printf
+	movl	28(%esp),	%eax
+	addl	%eax,	%eax
+	movl	%eax, 28(%esp)
+L2:
+	movl	28(%esp),	%eax
+	cmpl	20(%esp),	%eax
+	jle		L3
+	movl	$0, %eax
+	leave
+	ret
