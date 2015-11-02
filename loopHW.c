7d684341e3ca40da161b85195ce2b1b4acdad054
Vananh Vo <vananhvo.0108@gmail.com>
2015-11-01 19:48:09 -0800

    Update loopHW.c

--- loopHW.c
+++ loopHW.c
@@ -2,6 +2,17 @@
 *	This program mirrors the structure of "loopHW.s"
 *	@author: Vananh Vo
 *	Course: COMP B13 (FALL 2015)
-* Professor: Halrold Mendoza
-* Source: loopHW.c
+*	Professor: Harold Mendoza
+*	Source: loopHW.s
 */
+#include <stdio.h>
+int main (int argc, char **argv){
+	int inNum1 = atoi(argv[1]);
+	int inNum2 = atoi(argv[2]);
+	int outNum = inNum1;
+	while(outNum <= inNum2){
+		printf("%d",outNum);
+		outNum = outNum + outNum;
+	}
+	return 0;
+}
